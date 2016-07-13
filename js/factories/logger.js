149331c6c5ce807e5dcfecee8cbd41bfaf19549a
Malorie Kramer <mkramer2089@gmail.com>
2016-07-13 14:17:55 -0400

    curved title

--- js/factories/logger.js
+++ js/factories/logger.js
@@ -10,6 +10,8 @@
 		 		var call = $http({
 		          method: 'GET',
 		          url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=7cf16558d759d14815306832bd7341e2&units=imperial`,
+		          //how it is written without using template literal
+		           // url: "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=7cf16558d759d14815306832bd7341e2&units=imperial",
 		        });
 		 		
 		        return call;

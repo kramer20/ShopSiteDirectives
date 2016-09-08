4e2500ad5acde934872052b33c1f126f3aa9a250
Malorie Kramer <mkramer2089@gmail.com>
2016-09-08 12:56:12 -0400

    new index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
+var express = require('express'); 
+var app = express();
+app.use(express.static(__dirname));
 
-var static = require('node-static');
-var file = new static.Server();
-
-require('http').createServer(function(request, response) {
- request.addListener('end', function() {
-   file.serve(request, response);
- }).resume();
-}).listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
